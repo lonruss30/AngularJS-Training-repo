4d668e3a33242254f96f964be82f3e87255af72e
lonruss30 <qlonruss30@gmail.com>
2022-08-15 22:28:33 +0800

    commit

--- training-v1/web/signin/scripts/user.js
+++ training-v1/web/signin/scripts/user.js
@@ -8,7 +8,7 @@ app.controller('userCtrl', function ($scope) {
 app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "templates/signin.html",
+            templateUrl: "signin/templates/signin.html",
             controller: "signinCtrl"
         })
         .otherwise({ redirectTo: '/' });

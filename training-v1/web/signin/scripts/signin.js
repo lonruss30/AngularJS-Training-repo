07a1199492403878836a0c58a7a1a9cbb35ba00b
lonruss30 <qlonruss30@gmail.com>
2022-08-16 10:53:13 +0800

    commit

--- training-v1/web/signin/scripts/signin.js
+++ training-v1/web/signin/scripts/signin.js
@@ -1,5 +1,23 @@
 ﻿var app = angular.module('userApp');
 
-app.controller('signinCtrl', function ($scope) {
+app.controller('signinCtrl', function ($scope, services) {
+
+    $scope.signin = function () {
+        var data = {
+            "username": $scope.username,
+            "password": $scope.password
+        };
+
+        services.signin(data).then(function (result) {
+
+            console.log(result);
+            if (result.status == 200) {
+
+            }
+            else if (result.status == 400) {
+
+            }
+        });
+    }
 
 });
--- training-v1/web/signin/scripts/user.js
+++ training-v1/web/signin/scripts/user.js
@@ -14,3 +14,25 @@ app.config(function ($routeProvider, $locationProvider) {
         .otherwise({ redirectTo: '/' });
     $locationProvider.html5Mode(true);
 });
+
+app.service('services', function ($http, $window) {
+    services = {};
+    //Set the link once in upon open of the project
+    localStorage.setItem('link', 'https://mybasura-api.2sds.com/');
+
+    var surl = localStorage.getItem('link');
+
+    services.signin = function (data) {
+
+        var result = $http({
+            method: 'POST',
+            url: surl + 'api/my/basura/auth/users/signin', data: data
+        }).then(function (response) {
+            return response;
+        }, function (err) {
+            return err;
+        });
+        return result;
+    };
+    return services;
+});

c368a5fec60d88afa40088950a46e20fbde5b090
lonruss30 <qlonruss30@gmail.com>
2022-08-16 15:42:41 +0800

    commit

--- training-v1/web/users/scripts/list.js
+++ training-v1/web/users/scripts/list.js
@@ -1,5 +1,11 @@
 ﻿var app = angular.module('usersApp');
 
 app.controller('listCtrl', function ($scope) {
-    
+    $scope.page = 1;
+    $scope.selectvalue = '15';
+
+    services.getUsers().then(function (result) {
+        $scope.users = result;
+        console.log(result);
+    });
 });
--- training-v1/web/users/scripts/users.js
+++ training-v1/web/users/scripts/users.js
@@ -1,7 +1,10 @@
-﻿var app = angular.module('usersApp', ['ngRoute']);
+﻿var app = angular.module('usersApp', ['ngRoute','angularUtils.directives.dirPagination', 'ui.bootsrap']);
 
-app.controller('usersCtrl', function ($scope) {
-
+app.controller('usersCtrl', function ($scope, services) {
+    services.profile().then(function (result) {
+        $scope.profile = result;
+        console.log($scope.profile);
+    });
 });
 
 app.config(function ($routeProvider, $locationProvider) {
@@ -28,11 +31,66 @@ app.service('services', function ($http, $window) {
     var token = localStorage.getItem('token');
     var surl = localStorage.getItem('link');
 
-    services.signin = function (data) {
+    services.profile = function () {
+        var result = $http({
+            method: 'GET',
+            url: surl + 'api/my/basura/users/profile',
+            headers: { 'Authorization': 'Bearer ' + token }
+        }).then(function (response) {
+            return response.data;
+        }, function (err) {
+            return err;
+        });
+        return result;
+    };
+    
+    services.getUsers = function () {
+        var result = $http({
+            method: 'GET',
+            url: surl + 'api/my/basura/users',
+            headers: { 'Authorization': 'Bearer ' + token }
+        }).then(function (response) {
+            return response.data;
+        }, function (err) {
+            return err;
+        });
+        return result;
+    };
+
+    services.getUser = function (id) {
+        var result = $http({
+            method: 'GET',
+            url: surl + 'api/my/basura/users' + id,
+            headers: { 'Authorization': 'Bearer ' + token }
+        }).then(function (response) {
+            return response.data;
+        }, function (err) {
+            return err;
+        });
+        return result;
+    };
+
+    services.createUser = function (data) {
 
         var result = $http({
             method: 'POST',
-            url: surl + 'api/my/basura/auth/users/signin', data: data
+            url: surl + 'api/my/basura/users',
+            headers: {'Authorization': 'Bearer '+token},
+            data: data
+        }).then(function (response) {
+            return response;
+        }, function (err) {
+            return err;
+        });
+        return result;
+    };
+
+    services.updateUser = function (id, data) {
+        var result = $http({
+            method: 'PUT',
+            url: surl + 'api/my/basura/users' + id,
+            headers: { 'Authorization': 'Bearer ' + token },
+            data: data
         }).then(function (response) {
             return response;
         }, function (err) {
